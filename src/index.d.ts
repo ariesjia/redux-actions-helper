e09dd48d9aa61345f77549d02e43e1ae7732c6c5
ariesjia <ariesjia00@hotmail.com>
2017-06-19 17:43:38 +0800

    [0.0.23] update typings

--- src/index.d.ts
+++ src/index.d.ts
@@ -51,23 +51,34 @@ export function createAction<Payload, Meta, SuccessPayload, FailPayload>(
   IActionMeta<FailPayload, FailPayload>
 >
 
-export type Reducer<State> = (state: State, action: IAction<{}>) => State
+export type Reducer<State, Payload> = (state: State, action: IAction<Payload>) => State
 
-export interface IReducerNextThrow<State> {
-  next?(state: State, action: IAction<{}>): State
-  throw?(state: State, action: IAction<{}>): State
+export interface IReducerNextThrow<State, Payload> {
+  next?(state: State, action: IAction<Payload>): State
+  throw?(state: State, action: IAction<Payload>): State
 }
 
-export interface IReducerMap<State> {
-  [actionType: string]: Reducer<State> | IReducerNextThrow<State>
+export interface IReducerMap<State, Payload> {
+  [actionType: string]: Reducer<State, Payload> | IReducerNextThrow<State, Payload>
 }
 
 export function handleActions<State>(
-  reducerMap: IReducerMap<State>,
+  reducerMap: IReducerMap<State, {}>,
   initialState: State,
-): Reducer<State>
+): Reducer<State, {}>
+
+export function handleActions<State, Payload>(
+  reducerMap: IReducerMap<State, Payload>,
+  initialState: State,
+): Reducer<State, Payload>
 
 export function linstenActions<State>(
-  reducerMapListener: () => IReducerMap<State>,
+  reducerMapListener: () => IReducerMap<State, {}>,
   initialState: State,
-): Reducer<State>
+): Reducer<State, {}>
+
+
+export function linstenActions<State, Payload>(
+  reducerMapListener: () => IReducerMap<State, Payload>,
+  initialState: State,
+): Reducer<State, Payload>
